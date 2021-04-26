9b95fb74f66d9ec982a0be152bddce4e7024c7b5
Austinbreathesair <austinbreathesair@gmail.com>
2021-04-26 21:41:21 +0200

    feat: added recoveries column

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid md:grid-cols-2 gap-4">
+  <div class="grid md:grid-cols-3 gap-4">
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
       <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
       <div class="text-2xl mb-4">
@@ -21,6 +21,18 @@
       <div class="text-2xl mb-4">
         <span class="span font-bold">Total:</span>
         {{numberWithCommas(stats.TotalDeaths)}}
+      </div>
+    </div>
+
+    <div class="shadow-md bg-blue-300 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Recoveries</h3>
+      <div class="text-2xl mb-4">
+        <span class="span font-bold">New:</span>
+        {{numberWithCommas(stats.NewRecovered)}}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="span font-bold">Total:</span>
+        {{numberWithCommas(stats.TotalRecovered)}}
       </div>
     </div>
   </div>
