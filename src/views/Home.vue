873988207f007f8448dcf5046a6610f085383f57
Austinbreathesair <austinbreathesair@gmail.com>
2021-04-26 21:14:43 +0200

    feat: added specific country selector

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,15 @@
   <main v-if="!loading">
     <DataTitle :text="title"
                :dataDate="dataDate" />
+
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData"
+                   :countries="countries" />
+
+    <button @click="clearData"
+            v-if="stats.Country"
+            class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green 600">Clear Country</button>
   </main>
 
   <main class="flex flex-col align-center justify center text-center"
@@ -17,11 +26,15 @@
 
 <script>
 import DataTitle from '@/components/DataTitle';
+import DataBoxes from '@/components/DataBoxes';
+import CountrySelect from '@/components/CountrySelect';
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
+    DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -39,12 +52,24 @@ export default {
       const data = await res.json();
       return data;
     },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+
+    async clearData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
+    },
   },
   async created() {
     const data = await this.fetchCovidData();
     this.dataDate = data.Date;
     this.stats = data.Global;
-    this.countries = data.countries;
+    this.countries = data.Countries;
     this.loading = false;
   },
 };
